a0c6c4048d296730fd462daa1a54ee4c36081466
Max Lapan <max.lapan@gmail.com>
2016-07-17 20:39:26 +0200

    Save under uniq names

--- wifi_scanner/scan.py
+++ wifi_scanner/scan.py
@@ -1,5 +1,8 @@
 import os
+import sys
 import twain
+import datetime
+import lockfile
 from PIL import ImageFile
 
 # ================== Options to tweak
@@ -23,29 +26,105 @@ def detect_best_source(src_list):
     return src_list[0]
 
 
-def save_image(bmp_data, file_name):
+def save_image(bmp_data, dir_name):
     parser = ImageFile.Parser()
     parser.feed(bmp_data)
     image = parser.close()
+    file_name = os.path.join(dir_name, date_to_name(datetime.datetime.now()) + ".jpg")
     image.save(file_name)
 
 
+def date_to_name(dt):
+    """
+    Convert datetime object into human-readable string
+    representation used in dirnames and file names. It can be
+    reverted back to name_to_date function
+    :param dt:
+    :return:
+    """
+    return dt.strftime("%Y-%m-%d_%H-%M-%S")
+
+
+def name_to_date(s):
+    """
+    Convert back string representation to datetime object
+    :param s:
+    :return: None if wrong format detected
+    """
+    p = s.split("_")
+    if len(p) != 2:
+        return None
+
+    try:
+        p1 = map(int, p[0].split('-'))
+        p2 = map(int, p[1].split('-'))
+    except ValueError:
+        return None
+
+    if len(p1) != 3 or len(p2) != 3:
+        return None
+
+    return datetime.datetime(p1[0], p1[1], p1[2], p2[0], p2[1], p2[2])
+
+
+def find_last_file_datetime_and_dir(path):
+    """
+    Check all files in subdirs in a path and return datetime corresponding to that
+    :param path:
+    :return: tuple of datetime and string dir. If no file found, return None,None
+    """
+    max_dt = None
+    max_dir = None
+    for entry_path in os.listdir(path):
+        full_entry_path = os.path.join(path, entry_path)
+        if not os.path.isdir(full_entry_path):
+            continue
+        if name_to_date(entry_path) is None:
+            continue
+        for subentry_path in os.listdir(full_entry_path):
+            full_subentry_path = os.path.join(full_entry_path, subentry_path)
+            if not os.path.isfile(full_subentry_path):
+                continue
+            dt = name_to_date(subentry_path.split(".")[0])
+            if dt is None:
+                continue
+            if max_dt is None or max_dt < dt:
+                max_dt = dt
+                max_dir = full_entry_path
+    print max_dt, max_dir
+    return max_dt, max_dir
+
+
+def make_new_dir(base_path):
+    """
+    Create new timestamp-based directory
+    :param base_path:
+    :return: path name
+    """
+    path = os.path.join(base_path, date_to_name(datetime.datetime.now()))
+    os.makedirs(path)
+    return path
+
 if __name__ == "__main__":
-    # TODO: locking
-    # check directory
-    if not os.path.exists(FOLDER_TO_SAVE):
-        os.makedirs(FOLDER_TO_SAVE)
+    base_dir = os.path.dirname(sys.argv[0])
+    with lockfile.LockFile(os.path.join(base_dir, "lock")):
+        # check directory
+        if not os.path.exists(FOLDER_TO_SAVE):
+            os.makedirs(FOLDER_TO_SAVE)
 
-    # TODO: read directory structure and create new dir if needed
+        last_datetime, last_dir = find_last_file_datetime_and_dir(FOLDER_TO_SAVE)
+        max_delta = datetime.timedelta(minutes=NEW_FOLDER_MINUTES)
+        if last_datetime is None or datetime.datetime.now() - last_datetime > max_delta:
+            last_dir = make_new_dir(FOLDER_TO_SAVE)
 
-    sm = twain.SourceManager(0)
-    src = detect_best_source(sm.GetSourceList())
-    ss = sm.OpenSource(src)
-    ss.SetCapability(twain.ICAP_XRESOLUTION, twain.TWTY_FIX32, float(SCAN_RESOLUTION_DPI))
-    ss.SetCapability(twain.ICAP_YRESOLUTION, twain.TWTY_FIX32, float(SCAN_RESOLUTION_DPI))
-    ss.RequestAcquire(0, 0)
-    rv = ss.XferImageNatively()
-    if rv:
-        (handle, count) = rv
-        bmp_data = twain.DIBToBMFile(handle)
-        save_image(bmp_data, FOLDER_TO_SAVE + "/image.jpg")
+        sm = twain.SourceManager(0)
+        src = detect_best_source(sm.GetSourceList())
+        ss = sm.OpenSource(src)
+        ss.SetCapability(twain.ICAP_XRESOLUTION, twain.TWTY_FIX32, float(SCAN_RESOLUTION_DPI))
+        ss.SetCapability(twain.ICAP_YRESOLUTION, twain.TWTY_FIX32, float(SCAN_RESOLUTION_DPI))
+        ss.RequestAcquire(0, 0)
+        rv = ss.XferImageNatively()
+        if rv:
+            (handle, count) = rv
+            bmp_data = twain.DIBToBMFile(handle)
+            save_image(bmp_data, last_dir)
