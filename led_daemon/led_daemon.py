772db56d2e065374138f6d7d13715d6ca21bb10f
Max Lapan <max.lapan@gmail.com>
2016-08-18 06:55:38 +0200

    Preparations for daemon

--- led_daemon/led_daemon.py
+++ led_daemon/led_daemon.py
@@ -1,17 +1,39 @@
-#!/bin/env python3
+#!/usr/bin/env python3
+import os
+import sys
+import stat
+import os.path
+import argparse
+import logging as log
 
-from time import sleep
-import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT)
 
-def usleep(ms):
-    sleep(ms / 1000000.0)
+def sanity_checks(args):
+    if not os.path.isfile(args.led_tool):
+        log.error("Led tool not found (%s)" % args.led_tool)
+        return False
 
-while True:
-    GPIO.output(11, GPIO.HIGH)
-    GPIO.output(11, GPIO.HIGH)
-#    usleep(100.0)
-    GPIO.output(11, GPIO.LOW)
-#    usleep(200.0)
+    st = os.stat(args.led_tool)
+    mode = st.st_mode
+    if (mode & stat.S_IEXEC) == 0:
+        log.error("Led tool is not executable")
+        return False
+    if (mode & stat.S_ISUID) == 0:
+        log.error("Suid bit on let tool isn't set")
+        return False
+    if st.st_uid != 0:
+        log.error("Led tool must be chowned to root")
+        return False
+    return True
+
+
+if __name__ == "__main__":
+    log.basicConfig(format="%(asctime)-15s %(levelname)s %(message)s", level=log.INFO)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", type=int, default=8000, help="Port to listen")
+    parser.add_argument("--led_tool", type=str, default="./led_tool", help="Path to led_tool")
+    args = parser.parse_args()
+
+    if not sanity_checks(args):
+        sys.exit(1)
